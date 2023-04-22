9faf78ba52c8815331af5eab96eda6bbfdfe5091
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-22 12:13:13 +0300

    table view added.

--- SuperHeroBook/SuperHeroBook/ViewController.swift
+++ SuperHeroBook/SuperHeroBook/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        
+        return UITableViewCell()
+    }
+    
 
 }
 
